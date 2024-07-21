10f567c9a748ed167cd4e2b5a0a64eb6de66608f
Acteon-25 <jeancoaquira@hotmail.com>
2024-07-21 16:17:25 -0500

    NavBarResponsibe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useNavigate, Link } from "react-router-dom";
 import Logo from "../icons/Logo.png";
 import LineDrop from "../icons/LineDrop.svg";
+import Equis from "../icons/Equis.svg";
 import { useState } from "react";
 
 const Header = () => {
@@ -13,21 +14,30 @@ const Header = () => {
 
   return (
     <header className="flex flex-col md:flex-row justify-between items-center py-4 w-full mx-auto bg-gradient-to-r from-green-700 to-blue-500 bg-opacity-75 px-5">
-      <div className="flex justify-between items-center w-full md:w-auto">
+      <div className={`flex justify-between items-center w-full md:w-auto ${menuOpen ? 'px-5 fixed md:relative' : ''}`}>
         <Link to="/">
           <img src={Logo} alt="Logo" className="rounded-full w-12 h-12" />
         </Link>
-        <img
+        {menuOpen ? <img
+          src={Equis}
+          alt="Menu"
+          className="w-12 h-12 cursor-pointer mx-2 md:hidden block"
+          onClick={handleClick}
+        /> : <img
           src={LineDrop}
           alt="Menu"
           className="w-12 h-12 cursor-pointer mx-2 md:hidden block"
           onClick={handleClick}
-        />
+        />}
+
       </div>
       <nav
-        className={`flex-col items-center md:flex-row md:flex md:items-center gap-x-10 transition-all ease-in duration-500 ${
-          menuOpen ? "flex" : "hidden"
-        } md:flex`}
+        className={`
+          flex-col items-center pt-2
+          gap-x-10 transition-all ease-in duration-500 
+          md:flex-row md:flex md:items-center 
+          ${menuOpen ? "flex" : "hidden"
+          } md:flex`}
       >
         <NavLink
           className={({ isActive }) =>
